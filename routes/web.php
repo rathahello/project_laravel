fa3253a96b5d197981427574dbe654e081e0250f
sereyrotha.chann <sereyrotha.chann@pnc.lan>
2020-01-02 18:23:05 -0800

    laravel

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/start', function(){
+    return view('start');
+});
+Route::get('/lesson1',function(){
+    return view('lesson.lesson1');
+});
